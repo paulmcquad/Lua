71f94a90008b5ed732c29da602cb778e28011504
Paul McQuade <paulmcquad@gmail.com>
2016-12-25 20:37:04 +0000

    Started Lesson 5 - Functions
    add and print functions
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

---
+++ Lesson 5 - Functions/add2.lua
@@ -0,0 +1,7 @@
+-- Example:
+--dofile "add2.lua"
+--f(2, 4)
+--2 4
+
+function f (a, b) print (a, b) end
+
---
+++ Lesson 5 - Functions/print.lua
@@ -0,0 +1,9 @@
+print(8*9, 9/8)
+a = math.sin(3) + math.cos(10)
+print(a)
+print(os.date())
+print ("Hello World")     -->     print("Hello World")
+--> dofile 'print.lua'
+
+print [[a multi-line
+     message]] 
