cb73355e7d26f2710a6e57e75627d2bb8895a1be
Paul McQuade <paulmcquad@gmail.com>
2017-01-10 19:10:51 +0000

    Finished Lesson 8
    errors, functions and expressions
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

---
+++ Lesson 8 - Compilation, Execution, and Errors/foo.lua
@@ -0,0 +1,9 @@
+-- file 'foo.lua'
+function foo(x)
+	print(x)
+end
+
+--f = loadfile("foo.lua")
+--print(foo) --> nil
+--f() --> defines 'foo'
+--foo("ok") --> ok
---
+++ Lesson 8 - Compilation, Execution, and Errors/func.lua
@@ -0,0 +1,2 @@
+f = load("local a = 10; print (a + 20)")
+f()
